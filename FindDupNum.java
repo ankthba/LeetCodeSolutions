9fa4a06fe7b2b9dcbe7d493a24589a0fc2d20401
aniketh <anikethdb1@gmail.com>
2024-08-17 11:15:11 -0400

    Create FindDupNum.java

---
+++ FindDupNum.java
@@ -0,0 +1,21 @@
+class FindDupNum {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return num;
+            }
+            seen.add(num);
+        }
+        return -1;
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] == nums[j]) {
+        //             return nums[i];
+        //         }
+        //     }
+        // }
+        // return 0;
+    }
+}
